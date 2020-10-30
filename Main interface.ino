b6ffc05ead58e25b0ee08e030dc04f0999f53722
Mark-Caleca <73571657+Mark-Caleca@users.noreply.github.com>
2020-10-30 11:22:35 -0400

    Initial clone

---
+++ Main interface.ino
@@ -0,0 +1,372 @@
+//code written by Mark C at BlueStamp Engineering 2018 
+//3.15.19
+//libraries for screen + touchscreen
+#include "Adafruit_GFX.h"    // Core graphics library
+#include "SPI.h"
+#include "Adafruit_HX8357.h"
+#include "TouchScreen.h"
+//touchscreen 
+#define YP A2  
+#define XM A3 
+#define YM 7  
+#define XP 8   
+#define TS_MINX 110
+#define TS_MINY 80
+#define TS_MAXX 900
+#define TS_MAXY 940
+#define MINPRESSURE 2
+#define MAXPRESSURE 1000
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+#define BOXSIZE_w 160//148
+#define BOXSIZE_h 136
+// The display uses hardware SPI, plus #9 & #10
+#define TFT_RST -1  // dont use a reset pin, tie to arduino RST if you like
+#define TFT_DC 9
+#define TFT_CS 10
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
+//DHT library 
+#include "DHT.h"
+#define DHTPIN 2     // what digital pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+//Define DHT variables; otherwise functions won't recognize them
+float h=0;
+float t=0;
+float f=0;
+float hic=0;
+float hif=0;
+//photocell
+int photocellPin = 6;     // the cell and 10K pulldown are connected to a0
+int photocellReading;     // the analog reading from the analog resistor divider
+//UV sensor 
+#include "Wire.h"
+#include "Adafruit_SI1145.h"
+Adafruit_SI1145 uv = Adafruit_SI1145();
+//define UV variables
+float UVindex=0;
+//set cursor, used to center text
+unsigned long cursor1(int numb_right,int numb_up, int stringlength){
+  //numb is number of rectangles, stringlength is length of text
+  tft.setCursor(BOXSIZE_w *(numb_right-0.5) -(3*stringlength*3), BOXSIZE_h *numb_up);
+  //only works with default font, change (textsize*stringlength*3) for different text size (currently size is 3)
+}
+//defeault colors  
+#define txtcolor 0x001F //0x0FE9
+#define bkgndcolor HX8357_WHITE
+#define boxcolor 0xFE9 
+//bool definitions for later
+bool dht_selected = false;
+bool gps_selected = false;
+bool uv_selected = false;
+bool light_selected = false;
+bool thermal_selected = false;
+//GPS
+#include "Adafruit_GPS.h"
+#include "SoftwareSerial.h"
+SoftwareSerial mySerial(3, 5);
+Adafruit_GPS GPS(&mySerial);
+#define GPSECHO  false //true debugs, false does not
+boolean usingInterrupt = false;
+void useInterrupt(boolean);
+//run once at startup
+void setup() {
+  //multiplexer cd4053b
+  pinMode(6, OUTPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  //GPS
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);
+  useInterrupt(true);
+  //UV
+  digitalWrite(6,HIGH);
+  uv.begin();
+  //Serial
+  Serial.begin(9600);
+  //DHT
+  dht.begin();
+  //screen + touchscreen 
+  tft.begin(HX8357D);
+  //home screen load on startup 
+  home1();
+}
+TSPoint p = ts.getPoint();
+void loop(){
+  //touchscreen
+  //pressure detection (makes sure not too low)
+  TSPoint p = ts.getPoint();
+  if (p.z < MINPRESSURE || p.z > MAXPRESSURE) {
+     return;
+  }
+  p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
+  p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height()); 
+  //note: graphics originate from top left corner, touch originates from bottom left corner
+  //divided and multiplied numbers below to account for not registering touch correctly 
+  //detect for GPS pressed 
+  if ((p.y < (BOXSIZE_w /1.57)) && (p.x < (BOXSIZE_h*1.5))){
+    gps_selected = true;
+  }
+  //Other bottom center
+  else if ((p.y < ((BOXSIZE_w*2)/1.57)) && (p.x < (BOXSIZE_h*1.5))){
+    thermal_selected = true;
+  }
+  else if ((p.y < ((BOXSIZE_w*3)/1.57)) && (p.x < (BOXSIZE_h*1.5))){
+  }
+  //DHT
+  else if ((p.y < (BOXSIZE_w /1.57)) && (p.x < (2*BOXSIZE_h*1.5))){
+    dht_selected = true;
+  }
+  else if((p.y < ((BOXSIZE_w *2) / 1.57)) && (p.x <(2*BOXSIZE_h*1.5))){
+    uv_selected = true;
+  }
+  else if((p.y < ((BOXSIZE_w*3)/1.57)) && (p.x<(2*BOXSIZE_h*1.5))){
+     light_selected = true;   
+  }
+   else {Serial.print("top");}  
+ while (dht_selected == true){
+   dht_c();
+  }
+ while (gps_selected == true){
+   gps_c();
+ } 
+ while (light_selected == true){
+  light_c();
+ }
+ while(uv_selected == true){
+   uv_c();
+  }
+/* while(thermal_selected == true){
+  tft.fillScreen(HX8357_WHITE);
+  tft.setCursor(1,1);
+  tft.println("For the thermal camera, please run 'thermal_camera' - otherwise Arduino runs out of memory");
+  delay(2000);
+  home1();
+ }*/
+} 
+//GPS
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  #ifdef UDR0
+    if (GPSECHO)
+      if (c) UDR0 = c;  
+  #endif
+}
+void useInterrupt(boolean v) {
+  if (v) {
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  }
+  else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+uint32_t timer = millis();
+unsigned long gpsbk = HX8357_GREEN;
+unsigned long gpstxt = HX8357_RED;
+void gps_c() {
+  tft.setCursor(2,2);
+  tft.setTextColor(gpstxt);
+  tft.fillScreen(gpsbk);
+  if (GPS.newNMEAreceived()) {
+    if (!GPS.parse(GPS.lastNMEA()))   
+      return;
+  }
+  if (millis() - timer > 2000) { 
+    timer = millis(); 
+    GPS.read();
+    tft.print("Time:");
+    tft.print(GPS.hour, DEC); tft.print(":");
+    tft.print(GPS.minute, DEC); tft.print(':');
+    tft.print(GPS.seconds, DEC); tft.print('.');
+    tft.println(GPS.milliseconds);
+    tft.print("Date: ");
+    tft.print(GPS.month, DEC); tft.print('/');
+    tft.print(GPS.day, DEC); tft.print("/20");
+    tft.println(GPS.year, DEC);
+    tft.print("Fix: "); tft.print((int)GPS.fix);
+    tft.print(" quality: "); tft.println((int)GPS.fixquality); 
+    if (GPS.fix){
+      tft.print("Location: ");
+      tft.print(GPS.latitude, 4); tft.print(GPS.lat);
+      tft.print(", "); 
+      tft.print(GPS.longitude, 4); tft.println(GPS.lon);
+      tft.print("Location in degrees: ");
+      tft.print(GPS.latitudeDegrees, 4);
+      tft.print(", "); 
+      tft.println(GPS.longitudeDegrees, 4);
+      tft.print("Speed (knots): "); tft.println(GPS.speed);
+      tft.print("Angle: "); tft.println(GPS.angle);
+      tft.print("Altitude: "); tft.println(GPS.altitude);
+      tft.print("Satellites: "); tft.println((int)GPS.satellites);
+    }
+    else {
+      tft.print("No GPS fix, sorry!");
+    }
+    delay(2000);
+ }
+}
+//home screen
+void home1(){
+  //bool used later for if something is selected 
+  dht_selected = false;
+  gps_selected = false;
+  uv_selected = false;
+  light_selected = false;
+  thermal_selected = false;
+  tft.fillScreen(bkgndcolor);
+  cursor1(2,0,13);
+  tft.setTextColor(txtcolor);
+  tft.setTextSize(3);
+  tft.setTextWrap(true);
+  tft.setRotation(1);
+  tft.print("Welcome back!\n");
+  tft.setCursor(BOXSIZE_w *(2-0.5) -(3*16*3),24);
+  tft.print("Select a Sensor:");
+  tft.drawRect(0, 48, BOXSIZE_w, BOXSIZE_h, boxcolor);
+  cursor1(1,1,5);
+  tft.print("DHT22");
+  tft.drawRect(BOXSIZE_w, 48,BOXSIZE_w, BOXSIZE_h, boxcolor); 
+  cursor1(2,1,2);
+  tft.print("UV");
+  cursor1(3,1,5);
+  tft.print("Light");
+  tft.drawRect(BOXSIZE_w*2, 48,BOXSIZE_w, BOXSIZE_h, boxcolor); 
+  cursor1(1,2,3);
+  tft.print("GPS");
+  tft.drawRect(0, 48+BOXSIZE_h,BOXSIZE_w, BOXSIZE_h, boxcolor); 
+  cursor1(2,2,8);
+  tft.drawRect(BOXSIZE_w, 48+BOXSIZE_h,BOXSIZE_w, BOXSIZE_h, boxcolor); 
+  tft.print("Heat Cam");
+  cursor1(3,2,5);
+  tft.drawRect(BOXSIZE_w*2, 48+BOXSIZE_h,BOXSIZE_w, BOXSIZE_h, boxcolor); 
+  tft.print("Other");
+}
+//uv            
+unsigned long uvbkcolor = 0xFFE0;
+unsigned long uvtxtcolor = HX8357_RED;
+void uv_c(){
+   digitalWrite(6,HIGH);   //high is uv, low is thermal camera 
+   //uv variables to fix error uv index = 0
+   UVindex = uv.readUV();
+   UVindex /=100.0;
+   String UVadvice="";
+   if (UVindex < 2){
+    UVadvice="Low UV";
+    uvbkcolor = 0x0400;
+    uvtxtcolor = 0x07FF;
+   }
+   else if (UVindex < 5){
+    UVadvice="Moderate UV";
+    uvbkcolor = 0xEF40;
+    uvtxtcolor = 0xF800;
+   }
+   else if (UVindex < 7){
+    UVadvice="High UV";
+    uvbkcolor = 0xFAA0;
+    uvtxtcolor = 0x041F;
+   }
+   else if (UVindex < 10){
+    UVadvice="Very High UV";
+    uvbkcolor = 0xF800;
+    uvtxtcolor = 0xFDB6;
+   }
+   else if (UVindex > 10){
+    UVadvice="Extremely High UV";
+    uvbkcolor = 0x701C;
+    uvtxtcolor = 0x0408;
+   }
+   tft.fillScreen(uvbkcolor);
+   tft.setTextColor(uvtxtcolor);
+   tft.setCursor(2,2);
+   tft.print("Visible:");
+   tft.println(uv.readVisible());
+   tft.print("IR:");
+   tft.println(uv.readIR());
+   tft.print("UV index:");
+   tft.println(UVindex);
+   tft.print(UVadvice);
+   delay(2000);
+}
+//photocell "light"
+unsigned long lbkcolor = 0xFFE0;
+unsigned long ltxtcolor = 0xF800;
+void light_c(){
+  photocellReading = analogRead(photocellPin); 
+  String light_reading = "";
+  if (photocellReading < 10) {
+    light_reading=" Dark";
+    lbkcolor = HX8357_BLACK;
+    ltxtcolor = HX8357_WHITE;
+  } else if (photocellReading < 200) {
+    light_reading=" Dim";
+    lbkcolor = 0x8410;
+    ltxtcolor = 0x69A0;
+  } else if (photocellReading < 500) {
+    light_reading=" Light";
+    lbkcolor = 0xFFF0;
+    ltxtcolor = 0xFC10;
+  } else if (photocellReading < 800) {
+    light_reading=" Bright";
+    lbkcolor = 0xFFE0;
+    ltxtcolor = 0xF800;
+  } else {
+    light_reading=" Very bright";
+    lbkcolor = HX8357_WHITE;
+    ltxtcolor = HX8357_BLACK;
+  } 
+  tft.fillScreen(lbkcolor);
+  tft.setTextColor(ltxtcolor);
+  tft.setCursor(2,2);
+  tft.print("Photocell says it is:");
+  tft.println(photocellReading);
+  tft.print("This is:");
+  tft.println(light_reading);
+  delay(2000);
+}
+//DHT 
+unsigned long dhtbkcolor = 0x07E0; 
+unsigned long dhttxtcolor = HX8357_BLACK; 
+void dht_c() {
+    h = dht.readHumidity();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    f = dht.readTemperature(true);    
+    hif = dht.computeHeatIndex(f, h);
+    //change color based on heat index
+    if (hif > 60 && hif <80) {
+     dhtbkcolor = 0xE643;
+     dhttxtcolor = 0x8420;
+    }
+    else if (hif > 80){
+    dhtbkcolor = 0xFFE0;
+    dhttxtcolor = 0xF800;
+   }
+   else if (hif < 60 && hif > 40) {
+    dhtbkcolor = 0x07FF;
+    dhttxtcolor = 0xFFFF;
+   }
+   else if (hif < 40 && hif >20){
+    dhtbkcolor = 0x0019;
+    dhttxtcolor = 0xFFF6;
+   }
+   else if (hif < 20){
+    dhtbkcolor = 0x004B;
+    dhttxtcolor = 0x9CF3;
+   }
+    tft.fillScreen(dhtbkcolor);
+    tft.setTextColor(dhttxtcolor);
+    // Compute heat index, temperature, humidity in Fahrenheit (the default)
+    tft.setCursor(tft.width()/4.7,tft.height()/2-36);
+    tft.print("Humidity: "); tft.print(h); tft.print("%\n");
+    tft.setCursor(tft.width()/7.2,tft.height()/2-12);
+    tft.print("Temperature: "); tft.print(f); tft.print("*F\n");
+    tft.setCursor(tft.width()/6.2,tft.height()/2+12);
+    tft.print("Heat index: "); tft.print(hif); tft.print("*F\n");
+    delay(2000); //refresh screen with new data 
+}
---
+++ thermal_camera.ino
@@ -0,0 +1,234 @@
+#include "Adafruit_GFX.h"    // Core graphics library
+#include "Adafruit_HX8357.h" // Hardware-specific library
+#include "SPI.h" //built in library should be SPI.h 
+#include "Adafruit_SPITFT_Macros.h"
+#include <Wire.h>
+#include "Adafruit_AMG88xx.h"
+#include <avr/pgmspace.h>
+#define TFT_CS     10 //chip select pin for the TFT screen
+#define TFT_RST    -1  // you can also connect this to the Arduino reset
+                      // in which case, set this #define pin to 0!
+#define TFT_DC     9
+float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
+
+//low range of the sensor (this will be blue on the screen)
+#define MINTEMP 22
+
+//high range of the sensor (this will be red on the screen)
+#define MAXTEMP 34
+
+
+// 0 no optimse |1 pixels only written whe color changed| 2 pixels also optimized for most changed ones first (deals with noise issues)
+#define optimize 0
+
+#define interpolatemode 1 
+#define colorMode 1  //can be 0=64 color adafruit, 1=256 color map 0,1 use same 256 color table , just 0 has same value 4 times
+#define spi_optimized_st77xx false 
+#if spi_optimized_st77xx == true
+#define fillRectFast tft.fillRectFast
+#else
+#define fillRectFast tft.fillRect
+#endif
+#if colorMode == 1
+const PROGMEM uint16_t camColors[] =  
+{0x0004,0x0004,0x0005,0x0005,0x0006,0x0007,0x0007,
+0x0008,0x0008,0x0009,0x0009,0x000a,0x000b,0x000b,
+0x000c,0x000c,0x000d,0x000d,0x000e,0x000f,0x000f,
+0x0010,0x0010,0x0011,0x0012,0x0012,0x0013,0x0013,
+0x0014,0x0014,0x0015,0x0016,0x0016,0x0017,0x0017,
+0x0018,0x0018,0x0019,0x001a,0x001a,0x001b,0x001b,
+0x001c,0x001c,0x001d,0x001e,0x001e,0x001f,0x001f,
+0x001f,0x003e,0x003e,0x005d,0x005d,0x007c,0x009b,
+0x009b,0x00ba,0x00ba,0x00d9,0x00d8,0x00f8,0x0117,
+0x0117,0x0136,0x0136,0x0155,0x0174,0x0174,0x0193,
+0x0193,0x01b2,0x01b2,0x01d1,0x01f0,0x01f0,0x020f,
+0x020f,0x022e,0x024d,0x024d,0x026c,0x026c,0x028b,
+0x028b,0x02aa,0x02c9,0x02c9,0x02e8,0x02e8,0x0307,
+0x0307,0x0326,0x0345,0x0345,0x0364,0x0364,0x0383,
+0x03a3,0x03a2,0x03c1,0x03c1,0x03e0,0x03e0,0x0400,
+0x0c20,0x0c20,0x1440,0x1440,0x1c60,0x1c60,0x2480,
+0x2ca0,0x2ca0,0x34c0,0x34c0,0x3ce0,0x44e0,0x4500,
+0x4d20,0x4d20,0x5540,0x5540,0x5d60,0x6580,0x6580,
+0x6da0,0x6da0,0x75c0,0x75c0,0x7de0,0x8600,0x8600,
+0x8e20,0x8e20,0x9640,0x9640,0x9e60,0xa680,0xa680,
+0xaea0,0xaea0,0xb6c0,0xbec0,0xbee0,0xc700,0xc700,
+0xcf20,0xcf20,0xd740,0xdf40,0xdf60,0xe780,0xe780,
+0xefa0,0xefa0,0xf7c0,0xffe0,0xffe0,0xffe0,0xffe0,
+0xffc0,0xffc0,0xffa0,0xffa0,0xffa0,0xff80,0xff80,
+0xff60,0xff60,0xff60,0xff40,0xff40,0xff20,0xff20,
+0xff00,0xff00,0xff00,0xfee0,0xfee0,0xfec0,0xfec0,
+0xfec0,0xfea0,0xfea0,0xfe80,0xfe80,0xfe80,0xfe60,
+0xfe60,0xfe40,0xfe40,0xfe40,0xfe20,0xfe20,0xfe00,
+0xfe00,0xfde0,0xfde0,0xfde0,0xfdc0,0xfdc0,0xfda0,
+0xfda0,0xfda0,0xfd80,0xfd80,0xfd60,0xfd60,0xfd60,
+0xfd40,0xfd40,0xfd20,0xfd20,0xfd00,0xfd00,0xfce0,
+0xfcc0,0xfca0,0xfca0,0xfc80,0xfc60,0xfc40,0xfc40,
+0xfc20,0xfc00,0xfbe0,0xfbe0,0xfbc0,0xfba0,0xfb80,
+0xfb80,0xfb60,0xfb40,0xfb20,0xfb20,0xfb00,0xfae0,
+0xfac0,0xfac0,0xfaa0,0xfa80,0xfa60,0xfa60,0xfa40,
+0xfa20,0xfa00,0xfa00,0xf9e0,0xf9c0,0xf9a0,0xf9a0,
+0xf980,0xf960,0xf940,0xf940};
+#endif
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS,  TFT_DC, TFT_RST);
+
+Adafruit_AMG88xx amg;
+unsigned long delayTime;
+void setup() {
+  // Serial.begin(9600);
+  Serial.begin(115200);
+  tft.begin(HX8357D);
+  digitalWrite(6,LOW);
+  Serial.println(F("AMG88xx thermal camera!"));
+
+  tft.fillScreen(HX8357_BLACK);
+
+  #define    displayPixelWidth  tft.width() /8  //allows values to be hardcoded
+  #define    displayPixelHeight   tft.height() / 8
+
+  bool status;
+    
+  // default settings
+  status = amg.begin();
+  if (!status) {
+      Serial.println(F("Could not find a valid AMG88xx sensor, check wiring!"));
+      while (1);
+  }
+  
+  Serial.println( F("-- Thermal Camera Test --"));
+  
+  delay(100); // let sensor boot up
+
+}
+#define speedUpCompression 8
+uint8_t compressionnumber;//this counts pixels processed, if to many processed we make compression flux higher
+uint8_t compressionflux=50;//this number goes up and down depending on how many pixesl are changing (usually from noise)
+byte runagain=0;//used to refesh different amounts, for example we only process 1/4 of low quality pixes to make sure refesh happens besides with difference
+void loop() {
+  digitalWrite(6,LOW);
+  amg.readPixels(pixels);
+  
+  
+  byte i=0;
+  byte j=0; 
+  byte k=0;
+  
+  compressionnumber=0;// we reset each time thru
+  runagain++;runagain=runagain&15; //we run this routing to update a few times0,1, maybe more later on
+  while(k<8 ){ 
+    switch(k){// this allows j to do interleaving of page, when updates are slower, and improvese results of compressed bandwitdh
+    case 0: j=0; break;case 1: j=2; break;case 2: j=4; break;case 3: j=6; break;case 4: j=1; break;case 5: j=3; break;case 6: j=5; break;case 7: j=7; break;
+    }
+    if (i>7){i=0;k++;}//here we run code that makes sense of supersubsamples, sample is done when i=8,k=16
+    
+    
+    // |
+    // V i . j ------>direction of display sensor. helps when figuring out interpolation
+   #if colorMode <2
+    int colorIndex = map(pixels[i+j*8], MINTEMP, MAXTEMP, 0, 255); //we resuse this sample 
+    #else
+    //trying to average pixle data//
+     int colorIndex = map(pixels[i+j*8], MINTEMP, MAXTEMP, 0, 1023); //we resuse this sample 
+    #endif
+   
+    //we now compress and only update pixels on screen that change the most! it works and over time they all change, but to be sure force updates
+  
+    
+    #if interpolatemode == 0
+       //this is original for display code pixel placement
+       //there is also spi optimizations to allow spi bursts during pixel writes
+        tft.fillRect(displayPixelWidth *j, displayPixelHeight * i,displayPixelWidth, displayPixelHeight,(uint16_t)pgm_read_word_near(camColors+colorIndex));
+    #endif
+    
+    
+    
+    
+    #if interpolatemode > 0
+    //how it updates when more than 2x2 sub pixels
+    int pixelSizeDivide= 2*interpolatemode ; 
+    //[0][4][8][c] or [0][2] //order reverses depending on sample interpolateSampleDir 
+    //[1][5][9][d]    [1][3]
+    //[2][6][a][e]
+    //[3][7][b][f]
+    //fast subdivide low memory pixel enhancing code (by James Villeneuve 7-2018 also referencing MIT code and adafruit library for pixel placement code)
+         
+    int interpolatesampledir2=1; if(i<4){interpolatesampledir2=1;}else{interpolatesampledir2=-1;}//top(1) or bottom quadrunt (-1)
+    int interpolateSampleDir =1;// left  (1) or right quadrunt (-1)
+    int offset=0;
+    
+    //getsubpixelcolor()//side pixel,vertical pixil,LeftOrRigh,current pixel
+    if (j<4){interpolateSampleDir =1;}// we process left to right here . we need to change this so it scales with display resolution
+    else{interpolateSampleDir =-1;offset=displayPixelHeight-displayPixelHeight/pixelSizeDivide;}//if past half way on display we sample in other direction
+    //long timecount=micros();
+    
+    // getsubpixelcolor(pixelSizerDivide);//side pixel,vertical pixil,LeftOrRigh,current pixel
+    for (int raster_x=0;raster_x !=(pixelSizeDivide*interpolateSampleDir)  ;raster_x += 1*interpolateSampleDir){ //done with != instead of <> so i could invert direction ;)
+    for (int raster_y=0;raster_y != (pixelSizeDivide* interpolateSampleDir) ;raster_y += 1* interpolateSampleDir){ //0,1  
+    //we keep sample size from nieghbor pixels even when sample divides increase
+    
+    #if colorMode <2
+    int  tempcolor= map(pixels[(i+(interpolatesampledir2+raster_y/(pixelSizeDivide/2)))+(j+(raster_x/(pixelSizeDivide/2)))*8], MINTEMP, MAXTEMP, 0, 255);//we constrain color after subsampling
+    #else
+    int  tempcolor= map(pixels[(i+(interpolatesampledir2+raster_y/(pixelSizeDivide/2)))+(j+(raster_x/(pixelSizeDivide/2)))*8], MINTEMP, MAXTEMP, 0, 1023);//we constrain color after subsampling
+    #endif
+    
+    //next line changes the average of the color between the main pixel and the sub pixels
+    tempcolor=(( tempcolor*(pixelSizeDivide-raster_y)+ colorIndex*raster_y)/pixelSizeDivide+(tempcolor*(raster_x)+colorIndex*(pixelSizeDivide-raster_x))/pixelSizeDivide)/2;//subsample with real pixel and surounding pixels
+    
+    // prebuffer[AMG88xx_PIXEL_ARRAY_SIZE*4]
+    //Serial.print("x:");
+    //Serial.print(raster_x/2);
+    //Serial.print(" y:");
+    //Serial.println(raster_y/2);
+    //tempcolor=(tempcolor+ colorIndex)/2;//subsample with real pixel and surounding pixels
+    #if colorMode < 2
+    tempcolor=constrain(tempcolor,0,255);//subsample with real pixel and surounding pixels
+    #else
+    tempcolor=constrain(tempcolor,0,1023);//subsample with real pixel and surounding pixels
+    #endif
+    
+    
+    //formated line below for easier reading
+    //*******place pixels*************
+    fillRectFast(displayPixelWidth *j
+    +offset+ (interpolateSampleDir*displayPixelWidth/pixelSizeDivide)*(raster_x*interpolateSampleDir), //we reduce pixle size and step over in raster extra pixels created
+    displayPixelHeight* i+offset+(interpolateSampleDir*displayPixelHeight/pixelSizeDivide)*(raster_y*interpolateSampleDir),
+    displayPixelWidth/pixelSizeDivide,//we divide width of pixels. /2,4,8,16 is fast and compiler can just do bit shift for it
+    displayPixelHeight/pixelSizeDivide,//we divide hieght of pixels.
+    (uint16_t)pgm_read_word_near(camColors+tempcolor));  //we update pixel location with new subsampled pixel.
+    //would it make sense to subdivide to color of pixel directly? yes except camcolors is set with colors translated for heat.
+    //timertemp=micros()-timertemp;//we stop counter and subtract time of routine
+    //Serial.println(timertemp);
+       }//interpolatepixel_y 
+    }//interpolatepixel_x    
+    #endif
+    
+    //below ends the check buffer loop
+    #if optimize >0 
+  }
+  #endif
+  #if optimize >1
+      }
+  #endif
+     //we update each pixel data
+   #if optimise > 1
+   //pixelsbuf[i+j*8]=colorIndex;
+   #endif
+  
+  
+   i++;   
+  //old way
+    //         tft.fillRect(displayPixelHeight * (i /8), displayPixelWidth * (i % 8),
+      //    displayPixelHeight, displayPixelWidth, camColors[colorIndex]);
+   
+    }
+  
+  //we sample one time per frame 
+  if (compressionnumber>speedUpCompression){if (compressionflux<255) {compressionflux+=1;};
+  }//we increase range more slowly 
+  else{
+    if (compressionflux>0){compressionflux-=1;};
+  }
+  //here we rerun routine, but up resolution
+  
+
+}
